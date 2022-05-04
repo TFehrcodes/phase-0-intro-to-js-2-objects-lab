97591509ea87727808d7d2a6effa264824ebf3b7
Travis Fehr <travisefehr@gmail.com>
2022-05-03 17:04:31 -0700

    commit tree c5264cd2390971b23e0db2648f3b8c8ae3f5ee37

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// working on a program that will keep track of a company's employees
+// start with just employee name and street address
+
+const employee = {
+  name: '',
+  streetAddress: ''
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value
+  }
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+
+function deleteFromEmployeeByKey(empObj, key) {
+  // we want to return a new object that doesn't include the identified key-value pair
+  // use the spread operator
+  const newObj = {
+    ...empObj,
+  }
+  delete newObj[key];
+  return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(empObj, key) {
+  delete empObj[key];
+  return empObj;
+}
